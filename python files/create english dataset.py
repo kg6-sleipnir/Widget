60ff4badae3112ef90b54ec592c5b1f7513fae6f
kg6-sleipnir <45186108+kg6-sleipnir@users.noreply.github.com>
2022-03-30 09:34:33 -0400

    Commented the code for Create English Dataset.py
    This was an old file meant to scrape data from a large dataset for learning parts of speech in natural language processing that existed over many files. Although I don't think I need it anymore, I'm still going to keep it just in case.

--- python files/create english dataset.py
+++ python files/create english dataset.py
@@ -1,16 +1,22 @@
+#this file was meant to read over the giant amount of files from one of the CONLL 
+#conferences to extract all of the training data for part of speech training from the files.
 import glob
 
+#create files for writing the words and tags from each dataset to
 data = open("Words.txt", "w")
 tags = open("tags.txt", "w")
 
+#find all files in the dataset, each file ends in .tags
 trainingData = (glob.glob('./**/*.tags', recursive=True))
 
+#loop over dataset files
 for i in trainingData:
     readfile = open(i, "r")
     
     sentence = ""
     tagList = ""
     
+    #read each line in each file
     while True:
         try:
             line = readfile.readline()
@@ -23,7 +29,12 @@ for i in trainingData:
         tag = ""
         
         count = 0
+        
+        #there are two columns in each file one is the words and the other is the tags
         for i in line:
+            #check if the token is the tag of the period in the end of the sentence
+            #and write all of the words and tags to their respective files, then 
+            #move on to the next file
             if i == "." and count == 1:
                 tagList += i
                 print(sentence + "\n" + tagList)
@@ -33,17 +44,26 @@ for i in trainingData:
                 tagList = ""
                 count = 0
                 break
+                
+            #add the word to the sentence if the whitespace character
+            #separating the word and the tag is encountered
             elif i == "\t" and count == 0:
                 sentence += word + " "
                 word = ""
                 count += 1
+                
+            #add the tag to the taglist if the whitespace after the tag is encountered
             elif i == "\t" and count == 1:
                 tagList += tag + " "
                 tag = ""
                 count = 0
                 break
+            
+            #if the whitespace has not been encountered, add the current character to the current word
             elif count == 0:
                 word += i
+                
+            #if the whitespace has been encountered, add the current character to the current tag
             else:
                 tag += i
         
